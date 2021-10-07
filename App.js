2f7c08e72e71dea794d3706c072855fab6b05857
merdie <merdiemlb@gmail.com>
2021-10-07 17:11:06 +0100

    project initialization

--- App.js
+++ App.js
@@ -1,21 +1,75 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+// import { StatusBar } from "expo-status-bar";
+import React from "react";
+import {
+  useDimensions,
+  useDeviceOrientation,
+} from "@react-native-community/hooks";
+import {
+  Dimensions,
+  StyleSheet,
+  Text,
+  View,
+  StatusBar,
+  SafeAreaView,
+  Image,
+  TouchableWithoutFeedback,
+  TouchableOpacity,
+  TouchableHighlight,
+  Button,
+  Alert,
+  Platform,
+} from "react-native";
+import WelcomeScreen from "./app/screens/WelcomeScreen";
+import ViewImageScreen from "./app/screens/ViewImageScreen";
 
 export default function App() {
+  const { landscape } = useDeviceOrientation();
+  const handlePress = () => console.log("text press");
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <ViewImageScreen />
+    // <SafeAreaView style={styles.container}>
+
+    // <Text onPress={handlePress}>
+    //   Hello, this is my first test with react native and i want to test how a
+    //   text can be so so so long wow
+    // </Text>
+
+    /* <Text onPress={handlePress}>
+        I am testing this stuff and it is so amazing
+      </Text> */
+
+    /* <TouchableOpacity onPress={() => console.log("image tapped")}>
+        <Image
+          blurRadius={1}
+          fadeDuration={1000}
+          source={{
+            width: 200,
+            height: 300,
+            uri: "https://picsum.photos/200/300",
+          }}
+        />
+      </TouchableOpacity> */
+    /* <Button
+        title="Click"
+        onPress={() =>
+          Alert.alert("Modal", "You clicked on the button", [
+            { text: "Yes", onPress: () => console.log("Yes") },
+            { text: "NO", onPress: () => console.log("No") },
+          ])
+        }
+      /> */
+
+    /* <StatusBar style="auto" />
+    </SafeAreaView> */
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    paddingTop: Platform.OS === "android" ? StatusBar.currentHeight : 0,
+    // alignItems: "center",
+    // justifyContent: "center",
   },
 });
